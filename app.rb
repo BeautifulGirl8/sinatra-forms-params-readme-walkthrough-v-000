c27e2537299a46f4ac350d5e3e84336ce3e6d8c7
Vicky  Tee <vttak9@gmail.com>
2021-04-08 02:41:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ class App < Sinatra::Base
     erb :food_form
   end
   post '/food' do
+    params = {
+      :name => "Sam",
+      :favorite_food => "Green Eggs and Ham"
+    }
       params.to_s
     end
   # Add your post route and action below
-  params = {
-    :name => "Sam",
-    :favorite_food => "Green Eggs and Ham"
-  }
+  
 end
